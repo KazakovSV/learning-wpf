dcf2a8e165215cf1b8920f75090f422a7cd5f5a8
Stanislav Kazakov <kazakovsv@tut.by>
2020-07-19 02:11:44 +0300

    Initialize solution and add EightBall project

---
+++ LearningWPF/EightBall/AnswerGenerator.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace EightBall
+{
+    static class AnswerGenerator
+    {
+        private static string[] answers = new string[]
+        {
+            "Ask Again later","Can Not Predict Now","Without a Doubt",
+            "Is Decidely So","Concentrate and Ask Again","My Sources Say No",
+            "Yes, Definitely","Don't Count On It","Signs Point to Yes",
+            "Better Not Tell You Now","Outlook Not So Good","Most Likely",
+            "Very Doubtful","As I See It, Yes","My Reply is No","It Is Certain",
+            "Yes","You May Rely On It","Outlook Good","Reply Hazy Try Again"
+        };
+
+        public static string GetRandomAnswer() => answers[new Random().Next(0, answers.Length)];
+    }
+}
---
+++ LearningWPF/EightBall/MainWindow.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Windows;
+using System.Windows.Input;
+
+namespace EightBall
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void GetAnswerButton_OnClick(object sender, RoutedEventArgs e)
+        {
+            if (QuestionTextBox.Text.StartsWith("["))
+            {
+                QuestionTextBox.Text = "[Please enter a question here]";
+                return;
+            }
+
+            Cursor = Cursors.Wait;
+            System.Threading.Thread.Sleep(TimeSpan.FromSeconds(1));
+            AnswerTextBox.Text = AnswerGenerator.GetRandomAnswer();
+            Cursor = null;
+        }
+
+        private void QuestionTextBox_OnGotFocus(object sender, RoutedEventArgs e)
+        {
+            if (QuestionTextBox.Text.StartsWith("["))
+            {
+                QuestionTextBox.Text = string.Empty;
+            }
+        }
+    }
+}
